2d8d58e73fd4c0e995e00afec855d4f2ee4e6d32
Abigail Swarth <abigailswarth@gmail.com>
2018-07-09 16:36:40 -0400

    winner panel

--- app.js
+++ app.js
@@ -36,17 +36,40 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     document.querySelector('#current-' + activePlayer).textContent = roundScores;
   } else {
     //next player
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-    roundScores = 0;
+    nextPlayer()
+  }
+});
 
-    document.getElementById('current-0').textContent = '0';
-    document.getElementById('current-1').textContent = '0';
+document.querySelector('.btn-hold').addEventListener('click', function(){
+  //add current score to player's overall score
+  scores[activePlayer] += roundScores
 
-    //document.querySelector('.player-0-panel').classList.remove('active');
-    //document.querySelector('.player-1-panel').classList.add('active');
-    document.querySelector('.player-0-panel').classList.toggle('active');
-    document.querySelector('.player-1-panel').classList.toggle('active');
-
+  //update UI
+  document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer]
+  //check if player won the game by reaching 100 points
+  if (scores[activePlayer] >= 10) {
+    document.querySelector('#name-' + activePlayer).textContent = "Winner!";
     document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+  } else {
+    //nextPlayer
+    nextPlayer()
   }
 })
+
+
+function nextPlayer() {
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  roundScores = 0;
+
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  //document.querySelector('.player-0-panel').classList.remove('active');
+  //document.querySelector('.player-1-panel').classList.add('active');
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+
+  document.querySelector('.dice').style.display = 'none';
+}
